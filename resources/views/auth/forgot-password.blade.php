39253ab2376712bbbb9b2c0ad68db75fd366f88d
a352410303-cpu <a352410303@alumno.uttehuacan.edu.mx>
2026-03-01 17:23:53 -0600

    candado a enlaces ddedl favicon

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -5,8 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Recuperar Contraseña</title>
-    <link rel="shortcut icon" href="{{ asset('favicon.ico') }}?v=2" type="image/x-icon">
-    <link rel="icon" href="{{ asset('favicon.ico') }}?v=2" type="image/x-icon">
+    <link rel="shortcut icon" href="{{ secure_asset('favicon.ico') }}?v=2" type="image/x-icon">
+    <link rel="icon" href="{{ secure_asset('favicon.ico') }}?v=2" type="image/x-icon">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <style>
         @import url('https://fonts.googleapis.com/css?family=Montserrat:400,800');
