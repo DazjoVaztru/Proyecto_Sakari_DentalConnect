ef371351053fa7bf3122bf0fc43488f7ac7bdcc0
Abril0104 <a3524110212@alumno.uttehuacan.edu.mx>
2026-03-03 20:48:48 -0600

    Fix duplicate capitalization in AuthController
    Removed duplicate capitalization for 'apellido_materno' and 'nombre_clinica'.

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -31,8 +31,7 @@ class AuthController extends Controller
         $request->merge([
             'nombre' => StringHelper::capitalizeName($request->nombre),
             'apellido_paterno' => StringHelper::capitalizeName($request->apellido_paterno),
-            'apellido_materno' => StringHelper::capitalizeName($request->apellido_materno),
-            'nombre_clinica' => StringHelper::capitalizeName($request->nombre_clinica),
+            'apellido_materno' => StringHelper::capitalizeName($request->apellido_materno),,
             'rfc_clinica' => $request->rfc_clinica ? strtoupper(trim($request->rfc_clinica)) : null,
             'localidad' => StringHelper::capitalizeName($request->localidad),
             'estado_clinica' => StringHelper::capitalizeName($request->estado_clinica),
