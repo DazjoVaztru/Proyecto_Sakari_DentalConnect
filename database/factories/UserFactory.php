c869aa8f619f7a38545ef1eddc108fe90ed585f0
a352410303-cpu <a352410303@alumno.uttehuacan.edu.mx>
2026-03-10 02:45:18 -0600

    adicion del calendario de citas en el modal de pacientes para el agendamiento de citas

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,11 +24,11 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
+            // Cambiamos 'name' por 'nombre_completo'
+            'nombre_completo' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
-            'remember_token' => Str::random(10),
+            'rol' => 'doctor', // Campo obligatorio en tu tabla
+            'password' => static::$password ??= \Illuminate\Support\Facades\Hash::make('password'),
         ];
     }
 
@@ -37,7 +37,7 @@ class UserFactory extends Factory
      */
     public function unverified(): static
     {
-        return $this->state(fn (array $attributes) => [
+        return $this->state(fn(array $attributes) => [
             'email_verified_at' => null,
         ]);
     }
