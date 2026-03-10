c6c876041171f610a85b3181a17553bb7dbe3a66
a352410303-cpu <a352410303@alumno.uttehuacan.edu.mx>
2026-03-10 05:22:51 -0600

    fix: add Procfile for Railway deployment

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,7 +14,7 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
-        if ($this->app->environment('production')) {
+        if (str_contains(config('app.url'), 'https://')) {
             URL::forceScheme('https');
         }
     }
