9da7d8209b5987667f67d581db8c98f5662852a6
a3524110303-cpu <a3524110303@alumno.uttehuacan.edu.mx>
2026-03-12 18:40:56 -0600

    RUTAS DEL ODONTOGRAMA CORREGIDAS

--- routes/web.php
+++ routes/web.php
@@ -86,6 +86,9 @@ Route::middleware(['auth'])->group(function () {
     Route::post('/api/citas/{id}/completar', [DashboardController::class, 'completarCita'])->name('api.cita.completar');
     Route::get('/api/calendario/disponibilidad', [DashboardController::class, 'obtenerDisponibilidadMes'])->name('api.calendario');
     Route::get('/api/calendario/horas-ocupadas', [DashboardController::class, 'horasOcupadas'])->name('api.calendario.horas');
+    Route::get('/api/pacientes/{id}/odontograma', [OdontogramaController::class, 'getOdontograma'])->name('api.odontograma.paciente');
+    Route::post('/api/pacientes/{id}/odontograma', [OdontogramaController::class, 'updateOdontograma'])->name('api.odontograma.update');
+    
 
     // Citas
     Route::post('/citas', [CitaController::class, 'store'])->name('citas.store');
